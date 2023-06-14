80f6324ad6bd56859cf44194a0235885a529ad57
daxia <hahaonecat@gmail.com>
2023-06-14 19:43:12 +0000

    feat: Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import Button from "@/stories/Kdan Music Book/Button";
+import theme from "@/stories/Kdan Music Book/theme";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+
 export default function Home() {
   return (
-    <main>
-      Hello Cat.
-    </main>
+    <>
+      <Head>
+        <title>凱鈿音樂</title>
+      </Head>
+      <main className="w-full">
+        <ThemeProvider theme={theme}>
+          <Button variant="primary" size="large">
+            Primary Button
+          </Button>
+        </ThemeProvider>
+      </main>
+    </>
   )
 }
