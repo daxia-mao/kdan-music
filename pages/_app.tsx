678a0a9c8b223ced347fad06b071829363e2c98e
daxia <hahaonecat@gmail.com>
2023-06-15 14:29:23 +0000

    feat: 應用 ThemeProvider 到所有 Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import theme from "@/stories/Kdan Music Book/theme";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
