031df7436dd8833f37f757cd23643f12d0a72653
daxia <hahaonecat@gmail.com>
2023-06-17 16:19:56 +0000

    fix(next): 修正無法讀取 Spotify 提供的圖片網域問題

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['t.scdn.co'],
+  },
   compiler: {
     styledComponents: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
