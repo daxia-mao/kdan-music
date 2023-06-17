3ca7dc54a75b334886591569b4c9eaa7778b0865
daxia <hahaonecat@gmail.com>
2023-06-17 16:41:59 +0000

    fix: Update Vercel Domain

--- stories/Kdan Music Book/features/api/apiSlice.ts
+++ stories/Kdan Music Book/features/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { CategoryType } from '../../types';
 
-const productionUrl = `https://kdan-music.vercel.com/api/`
+const productionUrl = `https://kdan-music.vercel.app/api/`
 const devUrl = `http://localhost:3001/api/`;
 
 const getbaseUrl = () => {
