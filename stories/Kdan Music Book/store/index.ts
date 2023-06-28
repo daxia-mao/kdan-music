fadb4834d5d96e62f94f4bdb734c8e65ab0953ec
daxia <hahaonecat@gmail.com>
2023-06-28 17:17:44 +0000

    refactor:  API Fetch 策略改變，移除過時的 Code 。

--- stories/Kdan Music Book/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "../features/api/apiSlice";
-
-export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
-  middleware: (gDM) => gDM().concat(apiSlice.middleware)
-})
