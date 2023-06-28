72fef6bd78147450a226654c67537d4fe6ad8de3
daxia <hahaonecat@gmail.com>
2023-06-28 18:05:40 +0000

    feat: 新增 WhyUs 元件

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Header from "@/stories/Kdan Music Book/Header";
 import MusicPreview from "@/stories/Kdan Music Book/MusicPreview";
 import PopularCategories from "@/stories/Kdan Music Book/PopularCategories";
 import Trust from "@/stories/Kdan Music Book/Trust";
+import WhyUs from "@/stories/Kdan Music Book/WhyUs";
 import Head from "next/head";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <Trust />
         <MusicPreview />
         <PopularCategories />
+        <WhyUs />
       </main>
     </>
   );
