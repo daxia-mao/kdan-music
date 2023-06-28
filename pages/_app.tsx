049e47140690b825a66429d449ee79f13d27cea5
daxia <hahaonecat@gmail.com>
2023-06-28 18:28:47 +0000

    chore: add Vercel Web Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "styled-components";
 import theme from "@/stories/Kdan Music Book/theme";
 import type { AppProps } from "next/app";
+import Navbar from "@/stories/Kdan Music Book/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 import "@fontsource/inter";
 import "@fontsource/poppins";
-import Navbar from "@/stories/Kdan Music Book/Navbar";
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
