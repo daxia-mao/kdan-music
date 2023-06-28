6097b2cb7b7fd7e591eb07cb9e6898a764e3029a
daxia <hahaonecat@gmail.com>
2023-06-28 17:51:15 +0000

    fix: _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,10 @@ import theme from "@/stories/Kdan Music Book/theme";
 import type { AppProps } from "next/app";
 import "@fontsource/inter";
 import "@fontsource/poppins";
-import Navbar from "@/stories/Kdan Music Book/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
   );
