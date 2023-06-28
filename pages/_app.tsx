3992d87bda5e1c2fbdca17a1b80d2f619b8f8c46
daxia <hahaonecat@gmail.com>
2023-06-28 17:47:00 +0000

    chore: remove Sotre & Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,16 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "styled-components";
-import { Provider } from "react-redux";
 import theme from "@/stories/Kdan Music Book/theme";
 import type { AppProps } from "next/app";
 import "@fontsource/inter";
 import "@fontsource/poppins";
-import { store } from "@/stories/Kdan Music Book/store";
+import Navbar from "@/stories/Kdan Music Book/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
