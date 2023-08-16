549b490097c376a04a17e2cc6387b769353cc0f5
daxia <hahaonecat@gmail.com>
2023-08-16 16:01:41 +0000

    feat: 使所有 Story 可以連接到 Redux Store

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import type { Preview } from "@storybook/react";
 import "../styles/globals.css";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { store } from "../stories/Kdan Music Book/app/store";
 import theme from "../stories/Kdan Music Book/theme";
 import "@fontsource/poppins";
 import "@fontsource/inter";
@@ -18,9 +20,11 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <ThemeProvider theme={theme}>
-        <Story />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Story />
+        </ThemeProvider>
+      </Provider>
     ),
   ],
 };
